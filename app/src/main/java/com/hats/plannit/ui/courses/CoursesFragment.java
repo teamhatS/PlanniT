29dd93ac1bb28e63cc81b23b0613cd930b38f4e3
shadow88s <howardshowered@gmail.com>
2020-02-16 16:57:06 -0800

    -removed mainactivity FAB

--- app/src/main/java/com/hats/plannit/ui/courses/CoursesFragment.java
+++ app/src/main/java/com/hats/plannit/ui/courses/CoursesFragment.java
@@ -11,25 +11,54 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.hats.plannit.R;
+import com.hats.plannit.models.Course;
+import com.hats.plannit.ui.calendar.CalendarViewModel;
+
+import java.util.ArrayList;
 
 public class CoursesFragment extends Fragment {
 
     private CoursesViewModel coursesViewModel;
+    private ArrayList<Course> coursesList = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         coursesViewModel =
                 ViewModelProviders.of(this).get(CoursesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_courses, container, false);
-        final TextView textView = root.findViewById(R.id.text_courses);
-        coursesViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        RecyclerView customersRecyclerView =  root.findViewById(R.id.rv_courses);
+        addUITestData();
+        final CoursesAdapter cAdapter = new CoursesAdapter(coursesList, this.getContext());
+        customersRecyclerView.setAdapter(cAdapter);
+        customersRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        customersRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+//        coursesViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                cAdapter.notifyDataSetChanged();
+//            }
+//        });
+
+
         return root;
+    }
+
+    private void addUITestData(){
+        Course course1 = new Course
+                ("CECS 445", "M/W 3:30PM- 4:45PM", "ECS  Room 308");
+        Course course2 = new Course
+                ("CECS 327", "T/F 12:30PM- 1:45PM", "VEC  Room 408");
+        Course course3 = new Course
+                ("CECS 378", "M/W 1:20PM- 2:45PM", "ECS  Room 228");
+        coursesList.add(course1);
+        coursesList.add(course2);
+        coursesList.add(course3);
+
     }
 }
