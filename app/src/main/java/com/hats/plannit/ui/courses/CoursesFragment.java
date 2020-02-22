d96d1a8fef8c023443f6b7a8d902812125e32c95
shadow88s <howardshowered@gmail.com>
2020-02-21 17:15:45 -0800

    -added author

--- app/src/main/java/com/hats/plannit/ui/courses/CoursesFragment.java
+++ app/src/main/java/com/hats/plannit/ui/courses/CoursesFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -21,6 +22,10 @@ import com.hats.plannit.ui.calendar.CalendarViewModel;
 
 import java.util.ArrayList;
 
+/*
+ * author: Howard chen
+ */
+
 public class CoursesFragment extends Fragment {
 
     private CoursesViewModel coursesViewModel;
@@ -28,8 +33,7 @@ public class CoursesFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        coursesViewModel =
-                ViewModelProviders.of(this).get(CoursesViewModel.class);
+        coursesViewModel = new ViewModelProvider(this).get(CoursesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_courses, container, false);
         RecyclerView customersRecyclerView =  root.findViewById(R.id.rv_courses);
         addUITestData();
