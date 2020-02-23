4422e47d67ca4450afcf18d866b669dfe32799d6
Arin Babayan <arin.babayan@student.csulb.edu>
2020-02-23 12:00:13 -0800

    Search Layout

--- app/src/main/java/com/hats/plannit/ui/search/SearchFragment.java
+++ app/src/main/java/com/hats/plannit/ui/search/SearchFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,12 +12,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.hats.plannit.R;
+
+import java.util.ArrayList;
 
 public class SearchFragment extends Fragment {
 
     private SearchViewModel searchViewModel;
+    private ArrayList<Assignment> assignmentList = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +37,28 @@ public class SearchFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        final SearchView searchView = root.findViewById(R.id.search);
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setIconifiedByDefault(false);
+
+        Assignment a1 = new Assignment("CECS 491", "Project 1", "2/25/2020", "11:59 PM", "Description of Project !");
+        Assignment a2 = new Assignment("CECS 491", "Project 2", "3/25/2020", "11:59 PM", "Description of Project 2");
+        Assignment a3 = new Assignment("CECS 491", "Project 3", "4/25/2020", "11:59 PM", "Description of Project 3");
+        Assignment a4 = new Assignment("CECS 491", "Project 4", "5/25/2020", "11:59 PM", "Description of Project 4");
+        Assignment a5 = new Assignment("CECS 491", "Project 5", "6/25/2020", "11:59 PM", "Description of Project 5");
+        assignmentList.add(a1);
+        assignmentList.add(a2);
+        assignmentList.add(a3);
+        assignmentList.add(a4);
+        assignmentList.add(a5);
+
+        RecyclerView customersRecyclerView =  root.findViewById(R.id.rv_search);
+        final SearchAdapter searchAdapter = new SearchAdapter(assignmentList, this.getContext());
+        customersRecyclerView.setAdapter(searchAdapter);
+        customersRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        customersRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
         return root;
     }
 }
