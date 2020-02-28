eecba2a703a916294eca115d8b76e4c8173e3913
TienHuynhN <43525708+TienHuynhN@users.noreply.github.com>
2020-02-27 18:48:05 -0800

    signup_UI

---
+++ app/src/main/java/com/hats/plannit/ui/signup/SignUpView.java
@@ -0,0 +1,16 @@
+package com.hats.plannit.ui.signup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hats.plannit.R;
+
+public class SignUpView extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_view);
+    }
+}
