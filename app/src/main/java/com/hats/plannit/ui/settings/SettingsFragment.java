3bcfb96dace1e7d4cc0486667adabb55bcaa0a89
shadow88s <howardshowered@gmail.com>
2020-03-01 11:10:13 -0800

    - added OnClick Listeners for Account/Sound/Notification buttons - Added popup dialog fragments for each submenu setting

--- app/src/main/java/com/hats/plannit/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/hats/plannit/ui/settings/SettingsFragment.java
@@ -1,29 +1,39 @@
 package com.hats.plannit.ui.settings;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.hats.plannit.R;
 
 public class SettingsFragment extends Fragment {
 
     private SettingsViewModel settingsViewModel;
+    private Button buttonAccount;
+    private Button buttonSound;
+    private Button buttonNotification;
+    Dialog myDialog;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        buttonAccount = root.findViewById(R.id.btn_account_settings);
+        buttonSound = root.findViewById(R.id.btn_sound_settings);
+        buttonNotification = root.findViewById(R.id.btn_notification);
+
         settingsViewModel =
                 new ViewModelProvider(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
         
         settingsViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -31,7 +41,47 @@ public class SettingsFragment extends Fragment {
 
             }
         });
+
+        myDialog = new Dialog(getContext());
+
+        buttonAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAccountPopup(v);
+            }
+        });
+
+        buttonSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSoundPopup(v);
+            }
+        });
+
+        buttonNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotificationPopup(v);
+            }
+        });
+
+
         return root;
 
+    }
+
+    public void showAccountPopup(View v){
+        myDialog.setContentView(R.layout.fragment_account_settings);
+        myDialog.show();
+    }
+
+    public void showNotificationPopup(View v){
+        myDialog.setContentView(R.layout.fragment_notification_settings);
+        myDialog.show();
+    }
+
+    public void showSoundPopup(View v){
+        myDialog.setContentView(R.layout.fragment_sound_settings);
+        myDialog.show();
     }
 }
