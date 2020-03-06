0e8f578f01cc6c9eebf8ce085ff5d20d05067dc7
shadow88s <howardshowered@gmail.com>
2020-03-05 23:21:53 -0800

    -added DatePickerDialog for due date -added TimePickerDialog for due time

--- app/src/main/java/com/hats/plannit/ui/home/HomeFragment.java
+++ app/src/main/java/com/hats/plannit/ui/home/HomeFragment.java
@@ -2,6 +2,7 @@ package com.hats.plannit.ui.home;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -12,15 +13,16 @@ import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 import com.hats.plannit.R;
-
-
 import java.util.Calendar;
 
 /*
@@ -31,6 +33,14 @@ public class HomeFragment extends Fragment {
     private static final String TAG = "HomeFragment";
     private HomeViewModel homeViewModel;
     private FloatingActionButton addAssignmentFab;
+    private TextView dueDateTextView;
+    private TextView dueTimeTextView;
+    private TextInputLayout textInputCourseName;
+    private TextInputLayout textInputAssignmentName;
+    private TextInputLayout textInputAssignmentDesc;
+    private ImageView dueDateIconImageView;
+    private ImageView dueTimeIconImageView;
+    private TimePickerDialog.OnTimeSetListener onTimeSetListener;
     private DatePickerDialog.OnDateSetListener onDateSetListener;
     Dialog myDialog;
 
@@ -62,25 +72,15 @@ public class HomeFragment extends Fragment {
     }
 
     private void showAddAssignmentPopup(View v) {
-        ImageView dueDateIconImageView;
-        final TextView  dueDateTextView;
         myDialog.setContentView(R.layout.fragment_add_assignments);
         dueDateIconImageView = (ImageView) myDialog.findViewById(R.id.iv_due_date_icon);
         dueDateTextView = (TextView) myDialog.findViewById(R.id.tv_due_date);
+        dueTimeIconImageView = (ImageView) myDialog.findViewById(R.id.iv_due_time_icon);
+        dueTimeTextView = (TextView) myDialog.findViewById(R.id.tv_due_time);
         dueDateIconImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                Integer year = cal.get(Calendar.YEAR);
-                Integer month = cal.get(Calendar.MONTH);
-                Integer day = cal.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(getContext(),
-                        android.R.style.Theme_Material_Dialog_MinWidth, onDateSetListener,
-                        year, month, day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
-
+                showDatePickerPopup(v);
             }
         });
         onDateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -88,17 +88,51 @@ public class HomeFragment extends Fragment {
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 Log.d(TAG, "onDateSet: " + year + "/");
                 dueDateTextView.setText(year + "/" + month + "/" + dayOfMonth );
-
-
             }
+        };
 
+        dueTimeIconImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerPopup(v);
+            }
+        });
+
+        onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Log.d(TAG, "onDateSet: " + minute + "/");
+                dueTimeTextView.setText(hourOfDay + ":" + minute);
+            }
         };
 
         myDialog.show();
     }
 
     private void showDatePickerPopup(View v){
+        Calendar cal = Calendar.getInstance();
+        Integer year = cal.get(Calendar.YEAR);
+        Integer month = cal.get(Calendar.MONTH);
+        Integer day = cal.get(Calendar.DAY_OF_MONTH);
 
+        DatePickerDialog dialog = new DatePickerDialog(getContext(),
+                android.R.style.Theme_Material_Dialog_MinWidth, onDateSetListener,
+                year, month, day);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.GRAY));
+        dialog.show();
     }
+    
+    private void showTimePickerPopup(View v){
+        Calendar cal = Calendar.getInstance();
+        Integer hourOfDay = cal.get(Calendar.HOUR_OF_DAY);
+        Integer minute = cal.get(Calendar.MINUTE);
+
+        TimePickerDialog dialog = new TimePickerDialog(getContext(),
+                android.R.style.Theme_Material_Dialog_MinWidth, onTimeSetListener,
+                hourOfDay, minute, true);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.GRAY));
+        dialog.show();
+    }
+
 
 }
