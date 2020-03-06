4857cdd424f7530c6097ed267adbbad8bd960680
shadow88s <howardshowered@gmail.com>
2020-03-05 20:40:15 -0800

    - created add assignments fragment layout

--- app/src/main/java/com/hats/plannit/ui/home/HomeFragment.java
+++ app/src/main/java/com/hats/plannit/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.hats.plannit.ui.home;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,27 +12,50 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.hats.plannit.R;
+/*
+@author- Howard Chen
+ */
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private FloatingActionButton addAssignmentFab;
+    Dialog myDialog;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+        addAssignmentFab = root.findViewById(R.id.fab_add_assignment);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        myDialog = new Dialog(getContext());
+
+        addAssignmentFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddAssignmentPopup(v);
+            }
+        });
+
+
+
         return root;
     }
+
+    public void showAddAssignmentPopup(View v){
+        myDialog.setContentView(R.layout.fragment_add_assignments);
+        myDialog.show();
+    }
+
 }
