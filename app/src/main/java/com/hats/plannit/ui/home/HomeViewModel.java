91b0c1cba25530b6e4dd88220829510c7d4fa39d
shadow88s <howardshowered@gmail.com>
2020-03-06 08:42:23 -0800

    - implemented HomeView init method - added AddAssignment method - added LiveData getter method

--- app/src/main/java/com/hats/plannit/ui/home/HomeViewModel.java
+++ app/src/main/java/com/hats/plannit/ui/home/HomeViewModel.java
@@ -1,19 +1,37 @@
 package com.hats.plannit.ui.home;
 
+import android.content.Context;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.hats.plannit.models.Assignment;
+import com.hats.plannit.repos.AssignmentRepo;
+
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private static final String TAG = "HomeViewModel";
+    private MutableLiveData<List<Assignment>> mAssignmentList;
+    private AssignmentRepo aRepo;
 
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("Assignments");
+    public void init(){
+        if(aRepo != null){
+            return;
+        }
+        aRepo = AssignmentRepo.getInstance();
+        mAssignmentList = aRepo.getAssignments();
+        List<Assignment> currentAssignments = mAssignmentList.getValue();
+        mAssignmentList.postValue(currentAssignments);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public boolean addAssignment(Assignment newAssignment, Context context){
+        return aRepo.addAssignment(newAssignment, context);
     }
+    public MutableLiveData<List<Assignment>> getmAssignmentList() {
+        return mAssignmentList;
+    }
+
 }
