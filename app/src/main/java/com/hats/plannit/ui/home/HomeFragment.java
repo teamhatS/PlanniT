ff74c897380eaffda08533fa9cf8693bee572fc9
shadow88s <howardshowered@gmail.com>
2020-03-07 12:27:44 -0800

    -cleaned up code

--- app/src/main/java/com/hats/plannit/ui/home/HomeFragment.java
+++ app/src/main/java/com/hats/plannit/ui/home/HomeFragment.java
@@ -19,7 +19,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
