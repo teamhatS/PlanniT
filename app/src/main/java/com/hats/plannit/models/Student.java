047825b68a193710240090d7a95511963b567bc7
shadow88s <howardshowered@gmail.com>
2020-03-11 17:49:16 -0700

    - created Student model

--- app/src/main/java/com/hats/plannit/models/Student.java
+++ app/src/main/java/com/hats/plannit/models/Student.java
@@ -1,4 +1,80 @@
 package com.hats.plannit.models;
 
 public class Student {
+
+    private String studentID;//documentID
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String nickname;
+    private Boolean loggedIn;
+
+    public Student(){}
+
+    public Student(String studentID, String firstName, String lastName, String email, String password, String nickname, Boolean loggedIn) {
+        this.studentID = studentID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.loggedIn = loggedIn;
+    }
+
+    public Boolean getLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(Boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
