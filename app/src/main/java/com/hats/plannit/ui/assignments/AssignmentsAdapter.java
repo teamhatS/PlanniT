9db8de158136a0b5ca58358271bdce382f3b77c3
shadow88s <howardshowered@gmail.com>
2020-03-16 21:55:18 -0700

    - added long press delete assignments feature

--- app/src/main/java/com/hats/plannit/ui/assignments/AssignmentsAdapter.java
+++ app/src/main/java/com/hats/plannit/ui/assignments/AssignmentsAdapter.java
@@ -5,11 +5,13 @@ package com.hats.plannit.ui.assignments;
  * author: Howard chen
  */
 
+import android.app.Dialog;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -26,10 +28,12 @@ public class AssignmentsAdapter extends RecyclerView.Adapter<AssignmentsAdapter.
     private static final String TAG = "HomeAdapter";
     private List<Assignment> assignmentsArrayList;
     private Context mContext;
+    private AssignmentsViewModel assignmentsViewModel;
 
-    public AssignmentsAdapter(Context mContext, List<Assignment> assignmentsArrayList) {
+    public AssignmentsAdapter(Context mContext, List<Assignment> assignmentsArrayList, AssignmentsViewModel assignmentsViewModel) {
         this.assignmentsArrayList = assignmentsArrayList;
         this.mContext = mContext;
+        this.assignmentsViewModel = assignmentsViewModel;
     }
 
     @NonNull
@@ -44,6 +48,54 @@ public class AssignmentsAdapter extends RecyclerView.Adapter<AssignmentsAdapter.
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called.");
         ((ViewHolder)holder).bindView(position);
+
+        ((ViewHolder)holder)
+                .parentLayout
+                .setOnLongClickListener
+                        (new View.OnLongClickListener() {
+
+                            @Override
+                            public boolean onLongClick(View v) {
+                                final Assignment assignmentToDelete = assignmentsArrayList.get(position);
+                                final Dialog delDialog = new Dialog(mContext);
+                                delDialog.setContentView(R.layout.fragment_delete_dialog);
+                                Button yesButton = delDialog.findViewById(R.id.btn_deldialog_yes);
+                                Button noButton = delDialog.findViewById(R.id.btn_deldialog_no);
+                                TextView exitTextview = delDialog.findViewById(R.id.tv_deldialog_exit);
+                                TextView questionTextView = delDialog.findViewById(R.id.tv_deldialog_question);
+                                String question = "Do you want to delete "
+                                        + assignmentToDelete.getAssignmentName() + "?";
+                                questionTextView.setText(question);
+                                yesButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        assignmentsViewModel.delAssignment(
+                                                assignmentToDelete, mContext);
+                                        assignmentsArrayList.remove(position);
+                                        assignmentsViewModel.getmAssignmentList().setValue(assignmentsArrayList);
+                                        delDialog.dismiss();
+                                    }
+                                });
+                                delDialog.show();
+
+                                noButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        delDialog.dismiss();
+                                    }
+                                });
+
+                                exitTextview.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        delDialog.dismiss();
+                                    }
+                                });
+
+
+                                return false;
+                            }
+                        });
 
     }
 
@@ -65,6 +117,7 @@ public class AssignmentsAdapter extends RecyclerView.Adapter<AssignmentsAdapter.
 
         public ViewHolder(View itemView) {
             super(itemView);
+            parentLayout = itemView.findViewById(R.id.parent_layout_search);
             textViewCourseName = itemView.findViewById(R.id.tv_course_name);
             textViewAssignmentName = itemView.findViewById(R.id.tv_assignment_name);
             textViewAssignmentDate = itemView.findViewById(R.id.tv_assignment_date);
@@ -72,7 +125,6 @@ public class AssignmentsAdapter extends RecyclerView.Adapter<AssignmentsAdapter.
             textViewAssignmentTime = itemView.findViewById(R.id.tv_assignment_time);
             checkboxAssignmentComplete = itemView.findViewById(R.id.checkBox_assignment_complete);
             assignmentDescription = itemView.findViewById(R.id.assignment_description);
-            parentLayout = itemView.findViewById(R.id.relative_layout_home);
         }
 
         public void bindView(int position){
