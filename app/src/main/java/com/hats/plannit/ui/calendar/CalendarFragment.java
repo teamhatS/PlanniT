8b7ca7c416e76e7b7e41a182c72c351600125b1f
TienHuynhN <43525708+TienHuynhN@users.noreply.github.com>
2020-03-19 19:15:39 -0700

    weekly and daily calendar view

--- app/src/main/java/com/hats/plannit/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/hats/plannit/ui/calendar/CalendarFragment.java
@@ -1,65 +1,5 @@
-//package com.hats.plannit.ui.calendar;
-//
-//import android.os.Bundle;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ArrayAdapter;
-//import android.widget.CalendarView;
-//import android.widget.ListView;
-//import android.widget.TextView;
-//
-//import androidx.annotation.Nullable;
-//import androidx.annotation.NonNull;
-//import androidx.fragment.app.Fragment;
-//import androidx.lifecycle.Observer;
-//import androidx.lifecycle.ViewModelProviders;
-//
-//import com.hats.plannit.R;
-//
-//import org.w3c.dom.Text;
-//
-//import java.util.ArrayList;
-//
-//public class CalendarFragment extends Fragment {
-//
-////    private CalendarViewModel calendarViewModel;
-//    private CalendarView monthly_calendar;
-//    private TextView text;
-//
-//    public View onCreateView(@NonNull LayoutInflater inflater,
-//                             ViewGroup container, Bundle savedInstanceState) {
-////        calendarViewModel =
-////                ViewModelProviders.of(this).get(CalendarViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_calendar, container, false);
-//
-//        monthly_calendar = root.findViewById(R.id.monthly_calendar);
-//        text = root.findViewById(R.id.text);
-//
-//        monthly_calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
-//        {
-//            @Override
-//            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2)
-//            {
-//                String date = (i1 + 1) + "/" + i2 + "/" + i;
-//                text.setText(date);
-//            }
-//        });
-//
-////        final TextView textView = root.findViewById(R.id.monthly_calendar);
-////        calendarViewModel.getText().observe(this, new Observer<String>() {
-////            @Override
-////            public void onChanged(@Nullable String s) {
-////                textView.setText(s);
-////            }
-////        });
-//        return root;
-//    }
-//}
-
 package com.hats.plannit.ui.calendar;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -72,7 +12,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -88,7 +27,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
